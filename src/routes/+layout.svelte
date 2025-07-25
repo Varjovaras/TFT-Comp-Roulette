5a52542d42815ea6bbf37aa49a25522b752216a6
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-07-25 13:15:41 +0300

    remove unused neobrutalismcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,6 @@
 <script lang="ts">
 	import '../app.css';
 	import Navigator from '$lib/components/Navigator.svelte';
-	import { goto } from '$app/navigation';
-	import { page } from '$app/state';
-
-	let url = page.url;
-	console.log(url);
-
-	function navigateToRoulette() {
-		goto('/');
-	}
-
-	function navigateToBlamer() {
-		goto('/blamer');
-	}
 </script>
 
 <div class="min-h-screen bg-gray-900">
